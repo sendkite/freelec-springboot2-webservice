04f4ebb881b58e64042f9d3e2037d9ad66a79d19
sendkite <sendkite1@gmail.com>
2022-02-14 15:13:55 +0900

    feature: Create 추가

---
+++ src/main/java/com/example/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.example.book.springboot.service;
+
+import com.example.book.springboot.domain.posts.PostsRepository;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/main/java/com/example/book/springboot/web/PostsApiController.java
@@ -0,0 +1,22 @@
+package com.example.book.springboot.web;
+
+import com.example.book.springboot.service.PostsService;
+import com.example.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/vi/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
+        return postsService.save(requestDto);
+    }
+
+}
